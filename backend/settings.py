55e61758e99084372b1a5a2edfd7b831e04e1e03
Alejandro-Huaman <U201910144@upc.edu.pe>
2023-10-08 04:36:35 -0500

    add name db

--- backend/settings.py
+++ backend/settings.py
@@ -127,7 +127,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
+        'NAME': 'postgres',
         'USER': 'admin2',
         'PASSWORD': 'admin123',
         'HOST': 'database-2.ceyktdivclng.us-east-1.rds.amazonaws.com',
